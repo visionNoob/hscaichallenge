f3afbf1bf4206253b9541659cfdc984943ddc7d5
visionnoob <insurgent92@gmail.com>
2024-09-22 11:57:50 +0900

    update dist_train.sh

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -6,5 +6,5 @@ PORT=${PORT:-29500}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH
 echo $PYTHONPATH
-python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-    $(dirname "$0")/train.py $CONFIG --launcher pytorch ${@:4} --work-dir $WORKDIR
+/opt/conda/bin/python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
+    train.py $CONFIG --launcher pytorch ${@:4} --work-dir $WORKDIR
